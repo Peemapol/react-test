d33295adbe6476defde46db03c6d8223dbf435f4
peemapol.t <peemapol.t@arise.tech>
2024-10-16 13:33:41 +0700

    add globals css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import "./globals.css"
+
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
     <html lang="en">
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Counter from "./component/Counter";
+import Counter from "./component/Counter"
 
 export default function Home() {
   return (
-    <div>
+    <div className="w-full min-h-screen flex justify-center items-center bg-red-400">
       <Counter initialCount={0}/>
     </div>
-  );
+  )
 }
