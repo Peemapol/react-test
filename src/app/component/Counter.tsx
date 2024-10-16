c6723b523364deb0334114d2626607f2700ae55b
peemapol.t <peemapol.t@arise.tech>
2024-10-16 13:17:30 +0700

    counter component

---
+++ src/app/component/Counter.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+
+interface CounterProps {
+  initialCount?: number;
+}
+
+const Counter: React.FC<CounterProps> = ({ initialCount = 0 }) => {
+
+  const [count, setCount] = useState<number>(initialCount)
+
+  const increment = (): void => {
+    setCount((prev) => prev + 1)
+  }
+
+  const decrement = (): void => {
+    setCount((prev) => prev - 1)
+  }
+
+  const restart = (): void => {
+    setCount(0)
+  }
+
+  const switchSign = (): void => {
+    setCount((prev) => prev * -1)
+  }
+
+  return(
+    <div>
+      <h1>
+        Count: <span>{count}</span>
+      </h1>
+      <div>
+        <button onClick={increment}> Increment</button>
+        <button onClick={decrement}> Decrement</button>
+        <button onClick={restart}> Set 0</button>
+        <button onClick={switchSign}> Switch Sign</button>
+      </div>
+    </div>
+  )
+}
+
+export default Counter
